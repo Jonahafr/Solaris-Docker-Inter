1de37fb244f8a6a843373e684e0a2f3762179e55
Jonah, Chege <jonahchege@gmail.com>
2018-11-22 17:37:51 +0300

    Simple Zone Create Command

---
+++ Zonecreate.sh
@@ -0,0 +1,7 @@
+zclone: No such zone configured 
+Use 'create' to begin configuring a new zone. 
+zonecfg:zclone> create 
+create: Using system default template 'SYSdefault' 
+zonecfg:zclone> set zonepath=/zonefs/zclone 
+zonecfg:zclone> set ip-type=exclusive 
+zonecfg:zclone> exit 
